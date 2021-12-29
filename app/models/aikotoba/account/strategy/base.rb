d9de54c31ac2a06420a73fee8db272bf5768a037
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-12-29 13:45:40 +0900

    Added exception handling for arguments.

--- app/models/aikotoba/account/strategy/base.rb
+++ app/models/aikotoba/account/strategy/base.rb
@@ -1,32 +1,36 @@
 # frozen_string_literal: true
 
 module Aikotoba
-  class Account::Strategy::Base
-    class << self
-      def build_account_by(attributes)
-        raise NotImplementedError
-      end
+  module Account::Strategy
+    class InvalidAttributeError < StandardError; end
 
-      def find_account_by(credentials)
-        raise NotImplementedError
-      end
-
-      def confirmable?
-        raise NotImplementedError
-      end
-
-      def lockable?
-        raise NotImplementedError
-      end
-
-      private
-
-      def warning_not_supported
-        if Aikotoba::Account.enable_confirm? && !confirmable?
-          Rails.logger.warn("#{name} is not supported confirmable. Please set Aikotoba.enable_confirm to false or use a different strategy.")
+    class Base
+      class << self
+        def build_account_by(attributes)
+          raise NotImplementedError
         end
-        if Aikotoba::Account.enable_lock? && !lockable?
-          Rails.logger.warn("#{name} is not supported lockable. Please set Aikotoba.enable_lock to false or use a different strategy.")
+
+        def find_account_by(credentials)
+          raise NotImplementedError
+        end
+
+        def confirmable?
+          raise NotImplementedError
+        end
+
+        def lockable?
+          raise NotImplementedError
+        end
+
+        private
+
+        def warning_not_supported
+          if Aikotoba::Account.enable_confirm? && !confirmable?
+            Rails.logger.warn("#{name} is not supported confirmable. Please set Aikotoba.enable_confirm to false or use a different strategy.")
+          end
+          if Aikotoba::Account.enable_lock? && !lockable?
+            Rails.logger.warn("#{name} is not supported lockable. Please set Aikotoba.enable_lock to false or use a different strategy.")
+          end
         end
       end
     end
--- app/models/aikotoba/account/strategy/email_password.rb
+++ app/models/aikotoba/account/strategy/email_password.rb
@@ -3,64 +3,62 @@
 require "argon2"
 
 module Aikotoba
-  class Account::Strategy::EmailPassword < Account::Strategy::Base
-    def self.build_account_by(attributes)
-      email, password = attributes.values_at(:email, :password)
-      new.build_account_by(email: email, password: password)
-    end
-
-    def self.find_account_by(attributes)
-      email, password = attributes.values_at(:email, :password)
-      new.find_account_by(email: email, password: password)
-    end
-
-    def self.confirmable?
-      true
-    end
-
-    def self.lockable?
-      true
-    end
-
-    def build_account_by(email:, password:)
-      build_with_email_password(email, password)
-    end
-
-    def find_account_by(email:, password:)
-      account = find_by_email(email)
-      account if password_match?(account, password)
-    end
-
-    private
-
-    def find_by_email(email)
-      Aikotoba::Account.authenticatable.email_password.find_by(email: email)
-    end
-
-    def password_match?(account, password)
-      return false unless account
-      Argon2::Password.verify_password(password_with_pepper(password), account.password_digest)
-    end
-
-    def build_with_email_password(email, password)
-      Aikotoba::Account.new(email: email, password: password).tap do |resource|
-        resource.strategy = :email_password
-        password_digest = build_digest(resource.password)
-        resource.assign_attributes(password_digest: password_digest)
+  module Account::Strategy
+    class EmailPassword < Base
+      def self.build_account_by(attributes)
+        email, password = attributes.values_at(:email, :password)
+        new(email, password).build_account
       end
-    end
 
-    def build_digest(password)
-      generate_hash(password_with_pepper(password))
-    end
+      def self.find_account_by(attributes)
+        email, password = attributes.values_at(:email, :password)
+        new(email, password).find_account
+      end
 
-    def password_with_pepper(password)
-      "#{password}-#{Aikotoba.password_pepper}"
-    end
+      def self.confirmable?
+        true
+      end
 
-    def generate_hash(password)
-      argon = Argon2::Password.new(t_cost: Aikotoba.password_stretch)
-      argon.create(password)
+      def self.lockable?
+        true
+      end
+
+      def initialize(email, password)
+        @email, @password = email, password
+        raise InvalidAttributeError, "EmailPassword requires an email and password." if [@email, @password].any?(&:nil?)
+      end
+
+      def build_account
+        Aikotoba::Account.new(email: @email, password: @password).tap do |resource|
+          resource.strategy = :email_password
+          password_digest = build_digest(resource.password)
+          resource.assign_attributes(password_digest: password_digest)
+        end
+      end
+
+      def find_account
+        account = Aikotoba::Account.authenticatable.email_password.find_by(email: @email)
+        account if account && password_match?(account, @password)
+      end
+
+      private
+
+      def password_match?(account, password)
+        Argon2::Password.verify_password(password_with_pepper(password), account.password_digest)
+      end
+
+      def build_digest(password)
+        generate_hash(password_with_pepper(password))
+      end
+
+      def password_with_pepper(password)
+        "#{password}-#{Aikotoba.password_pepper}"
+      end
+
+      def generate_hash(password)
+        argon = Argon2::Password.new(t_cost: Aikotoba.password_stretch)
+        argon.create(password)
+      end
     end
   end
 end
