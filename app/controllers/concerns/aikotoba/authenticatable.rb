b1c9dd95deced35d7257633817edafe3ffd7136a
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2022-02-23 16:43:48 +0900

    If the unauthenticaticatable after login, cannot access the login required page.
    Even if there is already a session, verify that it can be authenticated,
    and if not, reset the session, in case the session is created and then locked by another browser etc.

--- app/controllers/concerns/aikotoba/authenticatable.rb
+++ app/controllers/concerns/aikotoba/authenticatable.rb
@@ -17,11 +17,18 @@ module Aikotoba
       reset_session
     end
 
+    # NOTE: Even if there is already a session, verify that it can be authenticated, and if not, reset the session,
+    # in case the session is created and then locked by another browser etc.
     def aikotoba_authenticate_by_session
-      Account.find_by(id: session[aikotoba_session_key])
+      account = Account.authenticatable.find_by(id: session[aikotoba_session_key])
+      account.tap { |account| reset_aikotoba_session unless account }
     end
 
     private
+
+    def reset_aikotoba_session
+      session[aikotoba_session_key] = nil
+    end
 
     def aikotoba_session_key
       Aikotoba.session_key
--- test/controllers/aikotoba/authenticatable_test.rb
+++ test/controllers/aikotoba/authenticatable_test.rb
@@ -43,4 +43,19 @@ class Aikotoba::AuthenticatableTest < ActionDispatch::IntegrationTest
     assert_nil session[Aikotoba.session_key]
     assert_redirected_to Aikotoba.after_sign_out_path
   end
+
+  test "If the unauthenticaticatable after login, cannot access the login required page." do
+    Aikotoba.confirmable = true
+    @account.update!(confirmed: true)
+    post aikotoba.new_session_path, params: {account: {email: @account.email, password: @account.password}}
+    assert_redirected_to Aikotoba.after_sign_in_path
+    assert_equal I18n.t(".aikotoba.messages.authentication.success"), flash[:notice]
+    @account.update!(confirmed: false)
+    get Aikotoba.after_sign_in_path
+    assert_redirected_to aikotoba.new_session_path
+    @account.update!(confirmed: true)
+    get Aikotoba.after_sign_in_path
+    assert_redirected_to aikotoba.new_session_path
+    Aikotoba.confirmable = false
+  end
 end
