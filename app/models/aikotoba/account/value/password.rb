4947092dd7f8bd9fd5c9327d5049d6393eeb3fc4
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2022-05-08 15:50:07 +0900

    Cutting out the hash algorithm into classes
    In order to make it easier to replace the hash algorithm of Password, the hash algorithm is cut out and injected into a class.

--- app/models/aikotoba/account/value/password.rb
+++ app/models/aikotoba/account/value/password.rb
@@ -1,48 +1,34 @@
 # frozen_string_literal: true
 
-require "argon2"
-
 module Aikotoba
   class Account::Value::Password
     LENGTH_RENGE = Aikotoba.password_length_range
 
     def initialize(
       value:,
-      pepper: Aikotoba.password_pepper
+      pepper: Aikotoba.password_pepper,
+      algorithm_class: Argon2
     )
       @value = value
       @pepper = pepper
+      @algorithm = algorithm_class.new(password: password_with_pepper(@value))
     end
 
     attr_reader :value
 
     def match?(digest:)
-      verify_password?(password_with_pepper(value), digest)
+      @algorithm.verify_password?(digest)
     end
 
     def digest
       return "" if value.blank?
-      generate_hash(password_with_pepper(value))
+      @algorithm.generate_hash
     end
 
     private
 
-    def verify_password?(password, digest)
-      Argon2::Password.verify_password(password, digest)
-    rescue Argon2::ArgonHashFail # NOTE: If an invalid digest is passed, consider it a mismatch.
-      false
-    end
-
     def password_with_pepper(password)
       "#{password}-#{@pepper}"
-    end
-
-    def generate_hash(password)
-      # NOTE: Adjusted to be OWASAP's recommended value by default.
-      # > Use Argon2id with a minimum configuration of 15 MiB of memory, an iteration count of 2, and 1 degree of parallelism.
-      # > https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#introduction
-      argon = Argon2::Password.new(t_cost: 2, m_cost: 14, p_cost: 1)
-      argon.create(password)
     end
   end
 end
---
+++ app/models/aikotoba/account/value/password/argon2.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "argon2"
+
+module Aikotoba
+  class Account::Value::Password::Argon2
+    def initialize(password:)
+      @password = password
+    end
+
+    def verify_password?(digest)
+      Argon2::Password.verify_password(@password, digest)
+    rescue Argon2::ArgonHashFail # NOTE: If an invalid digest is passed, consider it a mismatch.
+      false
+    end
+
+    def generate_hash
+      # NOTE: Adjusted to be OWASAP's recommended value by default.
+      # > Use Argon2id with a minimum configuration of 15 MiB of memory, an iteration count of 2, and 1 degree of parallelism.
+      # > https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#introduction
+      argon = Argon2::Password.new(t_cost: 2, m_cost: 14, p_cost: 1)
+      argon.create(@password)
+    end
+  end
+end
